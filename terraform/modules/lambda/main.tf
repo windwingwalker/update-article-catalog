0980c94d1e0d518382a749890b9073dd981d16f5
windWingWalker <elliot562k@gmail.com>
2022-05-06 15:52:45 +0800

    add tags

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -8,23 +8,24 @@ resource "aws_ecr_repository" "default" {
 }
 
 resource "aws_lambda_function" "default" {
+  # Neccessary
   function_name = var.app_name
+  package_type = "Image"
+  image_uri = "${aws_ecr_repository.default.repository_url}:${var.tag}"
+  role = var.lambda_role
+  publish = true
+
+  # Optional
+  depends_on = [
+    aws_ecr_repository.default,
+    aws_cloudwatch_log_group.default
+  ]
   timeout = 900
   environment {
     variables = {
       API_ID = var.api_id
     }
   }
-
-  package_type = "Image"
-  image_uri = "${aws_ecr_repository.default.repository_url}:${var.tag}"
-
-  role = var.lambda_role
-  depends_on = [
-    aws_ecr_repository.default,
-    aws_cloudwatch_log_group.default
-  ]
-  publish = true
 }
 
 resource "aws_cloudwatch_log_group" "default" {
